38512a138faa38d63e0f68352ca5ee98d8985d82
Edgar Herrera <edgarherrera@hey.com>
2021-09-14 07:59:30 -0500

    Add octicons from Github

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,9 +1,11 @@
+import { PackageIcon } from '@primer/octicons-react'
 import React from 'react'
 
 export const Home: React.FunctionComponent = () => {
   return (
     <>
       <h1>Home Page</h1>
+      <PackageIcon size={16} />
     </>
   )
 }
