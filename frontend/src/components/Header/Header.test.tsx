6f569ee9f70774d9d64cca536c02372971b14f6b
Edgar Herrera <edgarherrera@hey.com>
2021-09-14 11:31:01 -0500

    Add basic tests to Header and Footer component

---
+++ frontend/src/components/Header/Header.test.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import renderer from 'react-test-renderer'
+
+import { Header } from './Header'
+
+it('renders the Header', () => {
+  const tree = renderer.create(<Header />).toJSON()
+  expect(tree).toMatchSnapshot()
+})
