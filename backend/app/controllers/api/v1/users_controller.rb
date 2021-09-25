3153d1289d2c7e94ad2fc8d356b84b13d9e4bace
Luis Angel Ortega <angel-ortega12@hotmail.com>
2021-09-24 18:57:56 -0600

    Repositories - Updated controller to remove repos field

--- backend/app/controllers/api/v1/users_controller.rb
+++ backend/app/controllers/api/v1/users_controller.rb
@@ -12,13 +12,12 @@ module Api
           f.response :json # decode response bodies as JSON
         end
         user = conn.get("https://api.github.com/user?user=#{user_params}").body
-        repos = conn.get("https://api.github.com/user/repos?user=#{user_params}", { per_page: 100 }).body
         db_user = User.all.find { |u| u.github_id == user['id'] }
         if db_user.nil?
           db_user = User.create({ github_id: user['id'], login: user['login'], url: user['html_url'], name: user['name'],
-                                  email: user['email'], avatar_url: user['avatar_url'], repositories: repos })
+                                  email: user['email'], avatar_url: user['avatar_url']})
         end
-        render json: db_user.as_json.except('repositories')
+        render json: db_user.as_json
       end
 
       private
