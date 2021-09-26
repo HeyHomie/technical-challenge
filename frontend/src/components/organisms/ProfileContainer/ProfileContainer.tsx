c44a7401666133f50b54218bdae9bb8f68e29946
Alessandra <alessamica@gmail.com>
2021-09-25 23:39:43 -0400

    feat: implement search repositories by a given user

--- frontend/src/components/organisms/ProfileContainer/ProfileContainer.tsx
+++ frontend/src/components/organisms/ProfileContainer/ProfileContainer.tsx
@@ -15,15 +15,63 @@ const ProfileContainer: React.FC<ProfileContainerProps> = ({
   user,
   repositories
 }) => {
+  const [queryString, setQueryString] = React.useState('')
+  const [searched, setSearched] = React.useState(false)
+
+  const handleOnChange: any = async (
+    e: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    console.log(e.target.value)
+    setQueryString(e.target.value)
+    setSearched(true)
+  }
+
+  const filteredRepositories = repositories.filter((repo: Repository) => {
+    return repo.name.toLowerCase().includes(queryString.toLowerCase())
+  })
+
+  React.useEffect(() => {
+    if (queryString.length === 0) {
+      setSearched(false)
+    }
+  }, [queryString])
+
   return (
     <Wrapper>
       <Profile className='ProfileContainer'>
         <UserInfo user={user} />
         <div className='Repos'>
-          {repositories?.length > 0 &&
-            repositories.map((repo) => (
-              <RepositoryCard key={repo?.id} repository={repo} />
-            ))}
+          <div>
+            <ul>
+              <li>Overview</li>
+              <li>Repositories</li>
+              <li>Projects</li>
+              <li>Packages</li>
+            </ul>
+          </div>
+          <div>
+            <div>
+              <p>Search bar</p>
+              <input
+                type='text'
+                placeholder='Find a repository'
+                onChange={handleOnChange}
+              />
+            </div>
+            <span>Type</span>
+            <span>Language</span>
+            <span>Sort</span>
+            <span>New</span>
+          </div>
+          {!searched
+            ? repositories?.length > 0 &&
+              repositories.map((repo) => (
+                <RepositoryCard key={repo?.id} repository={repo} />
+              ))
+            : filteredRepositories?.length > 0 &&
+              filteredRepositories.map((repo) => (
+                <RepositoryCard key={repo?.id} repository={repo} />
+              ))}
         </div>
       </Profile>
     </Wrapper>
