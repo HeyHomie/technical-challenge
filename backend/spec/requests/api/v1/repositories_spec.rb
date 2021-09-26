a2e30356e2e58e8a67f13475befae8ccca995715
Luis Angel Ortega <angel-ortega12@hotmail.com>
2021-09-26 15:25:04 -0600

    Tests - Refactor test to fit new model and controller

--- backend/spec/requests/api/v1/repositories_spec.rb
+++ backend/spec/requests/api/v1/repositories_spec.rb
@@ -3,12 +3,29 @@
 require 'rails_helper'
 
 RSpec.describe 'Api::V1::Repositories', type: :request do
+  describe 'POST /create' do
+    it 'creates repositories for yknx4' do
+      get api_v1_users_path, params: { username: 'yknx4' }
+      user_id = JSON.parse(response.body)['id']
+
+      post  api_v1_user_repositories_path(user_id: user_id)
+      expect(response).to have_http_status(:created)
+    end
+  end
+  
   describe 'GET /index' do
-    it 'gets repositories for yknx4' do
-      get api_v1_user_repositories_path(user_id: 'yknx4')
+    it 'gets empty repositories for yknx4' do
+      get api_v1_users_path, params: { username: 'yknx4' }
+      user_id = JSON.parse(response.body)['id']
+      post  api_v1_user_repositories_path(user_id: user_id)
+
+      get api_v1_user_repositories_path(user_id: user_id)
       body = JSON.parse(response.body)
-      expect(response).to have_http_status(200)
-      expect(body.size).to eq(100)
+
+      p body
+
+      expect(response).to have_http_status(:ok)
+      expect(body['repositories'].size).to eq(5)
     end
   end
 end
