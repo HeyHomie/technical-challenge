63bc044a05d32471c02cdc473e126c70ce5b729a
MarioDena <enrique.dn@outlook.com>
2021-09-27 23:38:22 +0000

    feat: setup GraphQL testing

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,8 +1,12 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  if Rails.env.development?
+    mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "graphql#execute"
+  end
   post "/graphql", to: "graphql#execute"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # ! This is legacy code for v1 REST API that sadly no longer works.
   namespace :api do
     namespace :v1 do
       resources :users, only: [:index] do
