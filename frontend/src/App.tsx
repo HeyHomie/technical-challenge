756f4427867f819b7d69931ba1994a0c4d7d173f
MarioDena <enrique.dn@outlook.com>
2021-09-28 17:34:47 +0000

    feat: add basic routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,33 @@
 import React from 'react'
-import './App.css'
-import { AppRouter } from 'routes'
+import {
+  BrowserRouter,
+  Route,
+  Switch,
+  RouteComponentProps
+} from 'react-router-dom'
+import routes from 'config/routes'
 
 function App() {
-  return <AppRouter />
+  return (
+    <>
+      <BrowserRouter>
+        <Switch>
+          {routes.map((route, index) => {
+            return (
+              <Route
+                key={index}
+                path={route.path}
+                exact={route.exact}
+                render={(props: RouteComponentProps<any>) => {
+                  return <route.component {...props} {...route.props} />
+                }}
+              />
+            )
+          })}
+        </Switch>
+      </BrowserRouter>
+    </>
+  )
 }
 
 export default App
