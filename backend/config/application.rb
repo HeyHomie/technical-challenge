42962f91ada0458215275850c4566b0bca159739
Luis Angel Ortega <angel-ortega12@hotmail.com>
2021-09-28 16:06:45 -0600

    Sidekiq - Added sidekiq to the project

--- backend/config/application.rb
+++ backend/config/application.rb
@@ -39,6 +39,9 @@ module Backend
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.active_job.queue_adapter = :sidekiq
+    
     Oj.optimize_rails
   end
 end
