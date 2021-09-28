73b4e4771849269e8a01e742fd48d50605269ffe
MarioDena <enrique.dn@outlook.com>
2021-09-28 04:31:46 +0000

    feat: add all_users query

--- backend/app/graphql/types/query_type.rb
+++ backend/app/graphql/types/query_type.rb
@@ -1,5 +1,13 @@
+# frozen_string_literal: true
+
 module Types
   class QueryType < Types::BaseObject
+    field :all_users, [Types::UserType], null: false
+
+    def all_users
+      User.all
+    end
+
     field :user, Types::UserType, null: false do
       argument :login, String, required: true
     end
