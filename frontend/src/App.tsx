670b5cce749121403347ee22f0b481c6a6de9ace
MarioDena <enrique.dn@outlook.com>
2021-09-28 17:07:34 +0000

    chore: add basic files and types

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,8 @@
 import React from 'react'
 import './App.css'
-import { AppRouter } from './routes'
+import { AppRouter } from 'routes'
 
-
-function App () {
+function App() {
   return <AppRouter />
 }
 
