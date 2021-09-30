220d4d255133e75f1ef39581fd1d58b2ee33ec48
Luis Angel Ortega <angel-ortega12@hotmail.com>
2021-09-30 14:52:22 -0600

    Fix - Replaced create by find or create to prevent duplicates

--- backend/app/services/github_repositories_service.rb
+++ backend/app/services/github_repositories_service.rb
@@ -19,31 +19,25 @@ class GithubRepositoriesService
       )
 
       if response.success?
-        db_repos = Repository.where(user_id: user[:id]).select(:github_id)
-
-        response.body.each do |repo|
-          next if db_repos.include?(repo['id'])
-
-          Repository.create({
-                              github_id: repo['id'],
-                              url: repo['html_url'],
-                              name: repo['name'],
-                              user_id: user[:id],
-                              fork: repo['fork'],
-                              description: repo['description'],
-                              language: repo['language'],
-                              stars: repo['stargazers_count'],
-                              forks: repo['forks_count'],
-                              license: repo['license'] ? repo['license']['name'] : nil,
-                              last_updated: repo['updated_at'],
-                              archived: repo['archived'],
-                              private: repo['private']
-                            })
+        repos = response.body.map do |repo|
+          Repository.find_or_create_by(github_id: repo['id']) do |r|
+            r.github_id = repo['id']
+            r.url = repo['html_url']
+            r.name = repo['name']
+            r.user_id = user[:id]
+            r.fork = repo['fork']
+            r.description = repo['description']
+            r.language = repo['language']
+            r.stars = repo['stargazers_count']
+            r.forks = repo['forks_count']
+            r.license = repo['license'] ? repo['license']['name'] : nil
+            r.last_updated = repo['updated_at']
+            r.archived = repo['archived']
+            r.private = repo['private']
+          end
         end
 
-        created_repos = Repository.where(user_id: user[:id])
-
-        { message: created_repos, status: 200 }
+        { message: repos, status: 200 }
       else
         {
           message: "Error (#{response.status}) trying to get repository data from GitHub: #{response.body['message']} ",
