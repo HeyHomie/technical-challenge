d444d4cbf2b6188fa1446d39b8f14b7a90c8bccf
DavidMarioLC <davidmariolc.dev@gmail.com>
2021-10-12 16:36:08 -0500

    feat(components): add repositories component

---
+++ frontend/src/Components/repositories/Repositories.tsx
@@ -0,0 +1,33 @@
+import './repositories.css';
+import Repository from '../repository/Repository';
+import {IRepository} from '../../interfaces/repository.interfaces';
+
+interface Props{
+  repositories:IRepository[]
+}
+
+
+const Repositories = ({repositories}: Props) => {
+  return (
+    <div className="repositories"> 
+      {
+        repositories.map((repo,index)=>(
+          <Repository 
+             key={index}
+             name={repo.name} 
+             description={repo.description}
+             language={repo.language}
+             visibility={repo.visibility}
+             owner={repo.owner}
+             pushed_at={repo.pushed_at}
+             license={repo.license}
+             stargazers_count={repo.stargazers_count}
+             />
+        ))
+
+      }
+    </div>
+  )
+}
+
+export default Repositories
