7a0a7c0b2c708275acb7cfc93e3171ebe62c19fb
DavidMarioLC <davidmariolc.dev@gmail.com>
2021-10-12 16:32:53 -0500

    feat(component): add loading component

---
+++ frontend/src/Components/loading/Loading.tsx
@@ -0,0 +1,16 @@
+import "./loading.css";
+import {VscGithubAlt} from 'react-icons/vsc';
+const Loading = () => {
+  return (
+    <>
+    <div className="loadingPage">
+        <div className="loading">
+          <VscGithubAlt/>
+        </div>
+    </div>
+
+    </>
+  )
+}
+
+export default Loading
