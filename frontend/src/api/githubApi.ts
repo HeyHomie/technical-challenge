e83cec43c7269f4190870addabe3b15368f1816a
DavidMarioLC <davidmariolc.dev@gmail.com>
2021-10-12 21:09:14 -0500

    refactor(api): add params pagination

--- frontend/src/api/githubApi.ts
+++ frontend/src/api/githubApi.ts
@@ -7,14 +7,22 @@ interface Response{
     userExist:Boolean;
 }
 
+interface Props {
+  username:string,
+  numberItemByPage:number,
+  currentPage:number,
+  valueSelect:string
+}
 
-export const fetchGithubApi =async (username:string):Promise<Response> => {
+export const fetchGithubApi =async ({username,numberItemByPage,currentPage,valueSelect}:Props):Promise<Response> => {
+  
   const API_URL:string ="https://api.github.com/users";
 
   const ENDPOINT_USER:string = `${API_URL}/${username}`;
-  const ENDPOINT_REPOS:string =`${API_URL}/${username}/repos`;
-
-  const [responseUser,responseUserRepos] = await Promise.all([fetch(ENDPOINT_USER),fetch(ENDPOINT_REPOS)]);
+  const ENDPOINT_REPOS: string = `${API_URL}/${username}/repos?per_page=${numberItemByPage}&page=${currentPage}&sort=${valueSelect}`;
+  const [responseUser,responseUserRepos] = await Promise.all([
+    fetch(ENDPOINT_USER),
+    fetch(ENDPOINT_REPOS)]);
   
   const userExist = responseUser.ok;
   const user = await responseUser.json();
