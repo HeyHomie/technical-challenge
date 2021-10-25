ed0fbb487550dca62c47f7ef446cdea008a5cff5
Daniel Rejón <rejonpardenilla@gmail.com>
2021-10-25 15:20:59 -0500

    Add method to user to refresh repos data

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,5 +1,15 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  validates :login, :github_id, uniqueness: true
+  validates :login, uniqueness: true
+  validates :github_id, uniqueness: true, allow_nil: true
+
+  def refresh_repos_data
+    self.repositories = ::GithubRepos.fetch_from_user(login)
+  end
+
+  def refresh_repos_data!
+    refresh_repos_data
+    save!
+  end
 end
