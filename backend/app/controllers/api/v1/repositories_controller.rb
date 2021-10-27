56d05b26daac282f3f5180e0fdf59e6c59cdd0da
Daniel Rejón <rejonpardenilla@gmail.com>
2021-10-27 13:55:27 -0500

    Add elastic search for repositories

--- backend/app/controllers/api/v1/repositories_controller.rb
+++ backend/app/controllers/api/v1/repositories_controller.rb
@@ -5,7 +5,9 @@ module Api
     class RepositoriesController < ApplicationController
       def index
         user = User.find_or_fetch_from_github(login: user_params)
-        render json: user.repositories
+        fields = JSON.parse(search_params['fields'])
+        repos = Repo.search(search_params['query'], fields: fields, where: { user_id: user.id })
+        render json: repos.results
       rescue User::NoUserFound
         render json: nil, status: :not_found
       end
@@ -14,6 +16,14 @@ module Api
 
       def user_params
         params.require(:user_id)
+      end
+
+      def search_params
+        params.require(:search)
+              .permit(
+                :query,
+                fields: []
+              )
       end
     end
   end
