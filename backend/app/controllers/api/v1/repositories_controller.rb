faa320d3354816d989a8c80594eba2787da84fd5
Daniel Rejón <rejonpardenilla@gmail.com>
2021-10-27 14:09:50 -0500

    Fix search params to avoid being required

--- backend/app/controllers/api/v1/repositories_controller.rb
+++ backend/app/controllers/api/v1/repositories_controller.rb
@@ -5,8 +5,14 @@ module Api
     class RepositoriesController < ApplicationController
       def index
         user = User.find_or_fetch_from_github(login: user_params)
-        fields = JSON.parse(search_params['fields'])
-        repos = Repo.search(search_params['query'], fields: fields, where: { user_id: user.id })
+        search_hash = search_params['search']
+        if search_hash.present?
+          fields = JSON.parse(search_hash['fields'])
+          repos = Repo.search(search_hash['query'], fields: fields, where: { user_id: user.id })
+        else
+          repos = Repo.search(where: { user_id: user.id })
+        end
+
         render json: repos.results
       rescue User::NoUserFound
         render json: nil, status: :not_found
@@ -19,11 +25,14 @@ module Api
       end
 
       def search_params
-        params.require(:search)
-              .permit(
-                :query,
-                fields: []
-              )
+        params.permit(
+          search: [
+            :query,
+            {
+              fields: []
+            }
+          ]
+        )
       end
     end
   end
