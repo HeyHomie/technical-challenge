ccd16e721a7ddfde6e67eabe2472c3bd860a96a9
Francisco <francisco.marquez.solt@gmail.com>
2021-11-25 17:39:55 -0600

    feat: added components to handle loading and errors

---
+++ frontend/src/components/Error/index.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export const Error = (): JSX.Element => {
+  return <h1>Error</h1>
+}
---
+++ frontend/src/components/Loading/index.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export const Loading = (): JSX.Element => {
+  return <h1>Loading</h1>
+}
