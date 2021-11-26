d50e11567076fea79789a9998970b5eac1f030ba
Francisco <francisco.marquez.solt@gmail.com>
2021-11-26 15:16:10 -0600

    feat(footer): footer styling completed

--- frontend/src/components/Layout/Footer/index.tsx
+++ frontend/src/components/Layout/Footer/index.tsx
@@ -1,5 +1,29 @@
 import React from 'react'
+import './styles.css'
 
 export const Footer = (): JSX.Element => {
-  return <h1>Footer</h1>
+  return (
+    <div className='Footer--container'>
+      <div className='Footer__info'>
+        <p>Terms</p>
+        <p>Privacy</p>
+        <p>Security</p>
+        <p>Status</p>
+        <p>Docs</p>
+        <p>Contact</p>
+        <p>Pricing</p>
+        <p>API</p>
+        <p>Training</p>
+        <p>Blog</p>
+        <p>About</p>
+      </div>
+      <div className='Footer__logo'>
+        <img
+          src='https://cdn-icons-png.flaticon.com/512/25/25231.png'
+          alt='GitHub Logo'
+        />
+        <span>© 2021 GitHub, Inc.</span>
+      </div>
+    </div>
+  )
 }
