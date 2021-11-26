ee751a6f9bf2a0f6b6cae6b9780c36e04046db85
Francisco <francisco.marquez.solt@gmail.com>
2021-11-26 13:15:12 -0600

    feat(header): styled the header component

--- frontend/src/components/Layout/Header/index.tsx
+++ frontend/src/components/Layout/Header/index.tsx
@@ -1,5 +1,15 @@
 import React from 'react'
+import './styles.css'
 
 export const Header = (): JSX.Element => {
-  return <h1>Header</h1>
+  return (
+    <div className='Header--container'>
+      <h2 className='Header__title'>GitHub</h2>
+      <img
+        className='Header__logo'
+        src='https://cdn-icons-png.flaticon.com/512/25/25231.png'
+        alt='Github logo'
+      />
+    </div>
+  )
 }
