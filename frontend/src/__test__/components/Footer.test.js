bd3024c3414758924a6e2ba7e4d8f1499171ce11
Francisco <francisco.marquez.solt@gmail.com>
2021-11-27 16:49:56 -0600

    test: writed snapshots test for the Layout's components

--- frontend/src/__test__/components/Footer.test.js
+++ frontend/src/__test__/components/Footer.test.js
@@ -3,9 +3,18 @@ import { render  } from '@testing-library/react'
 import { Footer } from '../../components/Layout/Footer'
 import { create } from 'react-test-renderer'
 
-describe('Footer', () => {
+describe('Footer Component', () => {
   const footer = render(<Footer />)
   test('Render of the component Footer', () => {
-    expect(footer.length).toEqual(1);
+    expect(footer).toBeInTheDocument
   })
+});
+
+describe('Footer Snapshot', () => {
+  test('Comprobate UI from the component Footer', () => {
+    const footer = create(
+      <Footer />
+    );
+    expect(footer.toJSON()).toMatchSnapshot();
+  });
 });
