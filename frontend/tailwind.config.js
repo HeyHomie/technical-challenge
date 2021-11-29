24b1a446f9fca2491d584d0eed5438924085a18d
Alexis Barba <alexisbarba.2278@gmail.com>
2021-11-28 21:31:54 -0600

    config base colors

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,18 @@
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  mode: 'jit',
+  purge: ['./public/**/*.html', './src/**/*.{js,jsx,ts,tsx,vue}'],
   darkMode: 'class', // or 'media' or 'class'
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        primary: 'var(--primary)',
+        secondary: 'var(--secondary)'
+      },
+      textColor: {
+        primary: 'var(--text-primary)',
+        secondary: 'var(--text-secondary)'
+      }
+    }
   },
   variants: {
     extend: {}
