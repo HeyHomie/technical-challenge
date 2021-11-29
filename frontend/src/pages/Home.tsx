d4b68d46f7e4b1bd457a2eb9a880f4a00f8e44ea
Alexis Barba <alexisbarba.2278@gmail.com>
2021-11-28 23:37:20 -0600

    styles home grid

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState, FC } from 'react'
 import { getUser, getRepos } from 'api'
 import { useParams } from 'react-router-dom'
 import { IRepository, IUser } from 'types'
+import { UserCard } from 'components/ui'
 
 const Home: FC = () => {
   const { username } = useParams<{ username: string }>()
@@ -22,12 +23,16 @@ const Home: FC = () => {
   }
 
   return (
-    <>
-      <h1>{User.login}</h1>
-      {Repos.map((r) => (
-        <h2 key={r.id}>{r.name}</h2>
-      ))}
-    </>
+    <main className="flex flex-col px-4 mt-6 sm:flex-row">
+      <aside className="w-full sm:max-w-[256px] md:max-w-[296px] mt-4">
+        <UserCard user={User} />
+      </aside>
+      <div className="w-full">
+        {Repos.map((r) => (
+          <h2 key={r.id}>{r.name}</h2>
+        ))}
+      </div>
+    </main>
   )
 }
 
