20c6b86f6cd5978cb0281745ff52baac68e106aa
Alexis Barba <alexisbarba.2278@gmail.com>
2021-12-01 02:47:40 -0600

    refactor repolist

--- frontend/src/components/repo/RepoList.tsx
+++ frontend/src/components/repo/RepoList.tsx
@@ -1,18 +1,22 @@
 import { FC } from 'react'
 import { IRepository } from 'types'
 import RepoCard from './RepoCard'
+
 type RepoCardProps = {
   repos: IRepository[]
 }
+
 const RepoList: FC<RepoCardProps> = ({ repos }) => {
   return (
-    <ul className="text-sm ">
-      <li className="divide-y divide-accent-br">
-        {repos.map((repo) => (
-          <RepoCard repo={repo} />
-        ))}
-      </li>
-    </ul>
+    <div className="text-sm">
+      <ul>
+        <li className="divide-y divide-accent-br">
+          {repos.map((repo) => (
+            <RepoCard repo={repo} />
+          ))}
+        </li>
+      </ul>
+    </div>
   )
 }
 
