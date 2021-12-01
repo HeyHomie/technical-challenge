f08ddd37cb7a6823c2f9ca590becb9973d32f423
Alexis Barba <alexisbarba.2278@gmail.com>
2021-12-01 02:44:25 -0600

    update layout styles for responsive

--- frontend/src/components/common/Layout/Layout.tsx
+++ frontend/src/components/common/Layout/Layout.tsx
@@ -1,12 +1,17 @@
 import { FC } from 'react'
-import { Header } from 'components/common'
+import { Header, Footer } from 'components/common'
 
 const Layout: FC = ({ children }) => {
   return (
     <div className="dark">
-      <div className="w-screen min-h-screen text-primary bg-primary">
+      <div className="w-screen min-h-screen max-w-[100%] text-primary bg-secondary sm:bg-primary">
         <Header />
-        <main className="container mx-auto px-9">{children}</main>
+        <main
+          className="max-w-screen-xl min-h-full px-4 mx-auto md:px-9"
+          style={{ scrollbarColor: 'transparent' }}>
+          {children}
+        </main>
+        <Footer />
       </div>
     </div>
   )
