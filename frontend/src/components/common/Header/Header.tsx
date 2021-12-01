fab9036507c0ebcc3cbd8549d2f1d89b74ce1a26
Alexis Barba <alexisbarba.2278@gmail.com>
2021-12-01 02:43:35 -0600

    add search user and update styles on header

--- frontend/src/components/common/Header/Header.tsx
+++ frontend/src/components/common/Header/Header.tsx
@@ -3,52 +3,69 @@ import {
   MarkGithubIcon,
   BellIcon,
   PlusIcon,
-  ChevronDownIcon,
+  TriangleDownIcon,
   PersonIcon
 } from '@primer/octicons-react'
+import { useHistory } from 'react-router-dom'
+import { useRef } from 'react'
 
-const Header = () => (
-  <header className="h-16 sm:h-[60px] p-4 lg:px-8 sm:px-6 bg-secondary text-sm text-header">
-    <div className="flex items-center justify-between sm:hidden">
-      <ThreeBarsIcon size={24} />
-      <MarkGithubIcon size={32} />
-      <BellIcon size={24} />
-    </div>
-    <div className="items-center justify-between hidden sm:flex">
-      <div className="flex items-center gap-4">
+const Header = () => {
+  const history = useHistory()
+  const searchRef = useRef<HTMLInputElement>(null)
+
+  return (
+    <header className="h-16 md:h-[60px] p-4 lg:px-8 md:px-6 bg-secondary text-sm text-header">
+      <div className="flex items-center justify-between md:hidden">
+        <ThreeBarsIcon size={24} />
         <MarkGithubIcon size={32} />
-        <input
-          className="input max-w-[15.5rem]"
-          placeholder="Search or jump to..."
-        />
-        <ul className="flex gap-4 font-bold">
-          <li>
-            <a href="https://github.com/pulls">Pull Requests</a>
-          </li>
-          <li>
-            <a href="https://github.com/issues">Issues</a>
-          </li>
-          <li>
-            <a href="https://github.com/marketplace">Marketplace</a>
-          </li>
-          <li>
-            <a href="https://github.com/explore">Explore</a>
-          </li>
-        </ul>
+        <a href="github.com/notifications">
+          <BellIcon size={16} />
+        </a>
       </div>
-      <div className="flex gap-4">
-        <BellIcon size={16} />
-        <div className="flex items-center">
-          <PlusIcon size={16} />
-          <ChevronDownIcon size={10} className="my-auto" />
+      <div className="items-center justify-between hidden md:flex">
+        <div className="flex items-center w-full gap-4">
+          <MarkGithubIcon size={32} />
+          <div className="flex">
+            <input
+              className="input min-w-[15.5rem] max-w-[17rem] w-full"
+              placeholder="Search or jump to..."
+              ref={searchRef}
+            />
+            <button
+              className="w-auto btn"
+              onClick={() => history.push(searchRef?.current?.value || '/')}>
+              /{' '}
+            </button>
+          </div>
+          <ul className="flex gap-4 font-bold break-words">
+            <li>
+              <a href="https://github.com/pulls">Pull Requests</a>
+            </li>
+            <li>
+              <a href="https://github.com/issues">Issues</a>
+            </li>
+            <li>
+              <a href="https://github.com/marketplace">Marketplace</a>
+            </li>
+            <li>
+              <a href="https://github.com/explore">Explore</a>
+            </li>
+          </ul>
         </div>
-        <div className="flex items-center">
-          <PersonIcon size={20} className="border rounded-full" />
-          <ChevronDownIcon size={10} />
+        <div className="flex gap-4">
+          <BellIcon size={16} />
+          <div className="flex items-center">
+            <PlusIcon size={16} />
+            <TriangleDownIcon size={16} />
+          </div>
+          <div className="flex items-center">
+            <PersonIcon size={20} className="border rounded-full" />
+            <TriangleDownIcon size={16} />
+          </div>
         </div>
       </div>
-    </div>
-  </header>
-)
+    </header>
+  )
+}
 
 export default Header
