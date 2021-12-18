b564f13e653209824239c43ebdf6a93f69282dbe
fernandosg <fsg_tmp@hotmail.com>
2021-12-18 01:13:06 -0600

    Complemented tests for validations in the models repository and user

--- backend/spec/models/repository_spec.rb
+++ backend/spec/models/repository_spec.rb
@@ -2,12 +2,36 @@ require 'rails_helper'
 
 RSpec.describe Repository, type: :model do
   let(:repository) { Repository.new }
+  let(:user) { FactoryBot.create(:user) }
 
   describe 'name' do
     it 'is required' do
       repository.name = nil
+      repository.user = user
+
       repository.valid?
       expect(repository.errors[:name].size).to eq(1)
+    end
+
+    it 'is unique for the same user' do
+    	repository = FactoryBot.build(:repository)
+    	repository.user = user
+    	another_repository = repository.dup
+
+    	expect(repository.valid?).to be_truthy
+    	repository.save
+    	expect(another_repository.valid?).to be_falsy
+    end
+
+    it 'is not unique if is a different user' do
+    	repository = FactoryBot.build(:repository)
+    	repository.user = user
+    	another_repository = repository.dup
+    	another_repository.user = FactoryBot.create(:user)
+
+    	expect(repository.valid?).to be_truthy
+    	repository.save
+    	expect(another_repository.valid?).to be_truthy
     end
   end
 end
--- backend/spec/models/user_spec.rb
+++ backend/spec/models/user_spec.rb
@@ -3,7 +3,12 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  let(:user) { User.new }
+  let(:user) do 
+    user = User.new(login: Faker::Lorem.characters(number: 10))
+    user.name = user.login
+
+    user
+  end
 
   describe 'name' do
     it 'is required' do
@@ -11,11 +16,20 @@ RSpec.describe User, type: :model do
       user.valid?
       expect(user.errors[:name].size).to eq(1)
     end
+
+    it 'is unique' do
+      username = FactoryBot.create(:user).name
+      user.name = username
+
+      user.valid?
+      expect(user.errors.errors[0].type).to eq(:taken)
+    end
   end
 
   describe 'login' do
     it 'is required' do
       user.login = nil
+      
       user.valid?
       expect(user.errors[:login].size).to eq(1)
     end
