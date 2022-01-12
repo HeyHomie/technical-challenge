68e4b8e48d8cc247df99de01dbc35b19153b91b8
Jorge Moreno <jorgemoreno@192.168.1.11>
2022-01-12 16:03:54 -0600

    feat(dropdown, filters): base structure Dropdown filter
    Create the base structure of dropdown option select filters

--- frontend/src/Components/Filters/Filters.tsx
+++ frontend/src/Components/Filters/Filters.tsx
@@ -1,12 +1,21 @@
 import * as React from 'react'
+import Dropdown from './Dropdown'
 import Input from './Input'
 
 const Filters: React.FC = () => {
   return (
     <React.StrictMode>
-      <div>
-        <div>
+      <div className='filters-container'>
+        <div className='filters-container__input'>
           <Input />
+        </div>
+        <div className='filters-container__dropdown'>
+          <Dropdown />
+          <Dropdown />
+          <Dropdown />
+        </div>
+        <div className='filters-container__button'>
+          <button>New</button>
         </div>
       </div>
     </React.StrictMode>
