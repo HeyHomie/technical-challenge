6c839c121b2c6462cd43ce643b065d69f0a5e731
Jorge Moreno <jorgemoreno@MacBook-Pro-de-Jorge.local>
2022-01-12 00:49:35 -0600

    feat(profile.tsx): profile sidebar html structure
    Make the html structure of the profile sidebar, with icons, and sections like info, followers,
    county, highlights and avatar image

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,14 @@
 import * as React from 'react'
 import './App.css'
 import Navbar from './Components/Navbar/Navbar'
+import Profile from './Components/Profile/Profile'
 import { AppRouter } from './routes'
 
 const App: React.FC = (): JSX.Element => {
   return (
     <>
       <Navbar />
+      <Profile />
       <AppRouter />
     </>
   )
