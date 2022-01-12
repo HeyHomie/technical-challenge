1e3f71da88e270b1d9b0b2858ec82204c9a2cf53
Jorge Moreno <jorgemoreno@192.168.1.11>
2022-01-12 15:12:35 -0600

    feat(filter, input): filter container and Input component
    Create a filter container with input component without functions yet

---
+++ frontend/src/Components/Filters/Filters.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react'
+import Input from './Input'
+
+const Filters: React.FC = () => {
+  return (
+    <React.StrictMode>
+      <div>
+        <div>
+          <Input />
+        </div>
+      </div>
+    </React.StrictMode>
+  )
+}
+
+export default Filters
---
+++ frontend/src/Components/Filters/Input.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react'
+
+const Input: React.FC = () => {
+  return (
+    <React.StrictMode>
+      <input placeholder='Find a repository' type='text' />
+    </React.StrictMode>
+  )
+}
+
+export default Input
