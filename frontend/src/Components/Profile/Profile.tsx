27ee4996bf8bb3facc182bfc5d099002529196ff
Jorge Moreno <jorgemoreno@MacBook-Pro-de-Jorge.local>
2022-01-12 02:15:16 -0600

    feat(profile.tsx profile.scss): profile sidebar styles
    Profile sidebar styles in desktop version, add icons, variable colors and queries

--- frontend/src/Components/Profile/Profile.tsx
+++ frontend/src/Components/Profile/Profile.tsx
@@ -1,10 +1,11 @@
 import * as React from 'react'
+import '../Profile/Profile.scss'
 
 const Profile: React.FC = () => {
   return (
     <React.StrictMode>
-      <aside>
-        <div>
+      <aside className='aside'>
+        <div className='aside-avatar'>
           <a href='/'>
             <img
               src='https://cdn-icons-png.flaticon.com/512/147/147144.png'
@@ -14,25 +15,27 @@ const Profile: React.FC = () => {
             />
           </a>
         </div>
-        <div>
-          <h1>Profile Name</h1>
-          <h2>Profile Username</h2>
+        <div className='aside-names'>
+          <h1 className='aside-names__name'>Profile Name</h1>
+          <h2 className='aside-names__username'>Profile Username</h2>
         </div>
-        <div>
-          <div>
-            <span>Profile Description</span>
+        <div className='aside-information'>
+          <div className='aside-information__description'>
+            <span>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+            </span>
           </div>
-          <div>
+          <div className='aside-information__button'>
             <button>Edit Profile</button>
           </div>
-          <div>
-            <a className='' href='/'>
+          <div className='aside-information__follow'>
+            <a className='aside-information__follow_followers' href='/'>
               <svg
                 aria-hidden='true'
-                height='16'
+                height='18'
                 viewBox='0 0 16 16'
                 version='1.1'
-                width='16'
+                width='18'
                 data-view-component='true'
                 className='octicon octicon-people'
               >
@@ -43,17 +46,17 @@ const Profile: React.FC = () => {
               </svg>
               <span className=''>14</span> followers
             </a>
-            <a className='' href='/'>
+            <a className='aside-information__follow_following' href='/'>
               <span className=''>12</span>following
             </a>
           </div>
-          <div>
+          <div className='aside-information__location'>
             <svg
               className='octicon octicon-location'
               viewBox='0 0 16 16'
               version='1.1'
-              width='16'
-              height='16'
+              width='18'
+              height='18'
               aria-hidden='true'
             >
               <path
@@ -64,7 +67,7 @@ const Profile: React.FC = () => {
             <span>Location, User</span>
           </div>
         </div>
-        <div>
+        <div className='aside-achievements'>
           <h3>Achievements</h3>
           <a href='https://archiveprogram.github.com/'>
             <img
@@ -74,23 +77,25 @@ const Profile: React.FC = () => {
             />
           </a>
         </div>
-        <div>
+        <div className='aside-highlights'>
           <h3>Highlights</h3>
-          <svg
-            aria-hidden='true'
-            height='16'
-            viewBox='0 0 16 16'
-            version='1.1'
-            width='16'
-            data-view-component='true'
-            className='octicon octicon-star UnderlineNav-octicon hide-sm'
-          >
-            <path
-              fillRule='evenodd'
-              d='M8 .25a.75.75 0 01.673.418l1.882 3.815 4.21.612a.75.75 0 01.416 1.279l-3.046 2.97.719 4.192a.75.75 0 01-1.088.791L8 12.347l-3.766 1.98a.75.75 0 01-1.088-.79l.72-4.194L.818 6.374a.75.75 0 01.416-1.28l4.21-.611L7.327.668A.75.75 0 018 .25zm0 2.445L6.615 5.5a.75.75 0 01-.564.41l-3.097.45 2.24 2.184a.75.75 0 01.216.664l-.528 3.084 2.769-1.456a.75.75 0 01.698 0l2.77 1.456-.53-3.084a.75.75 0 01.216-.664l2.24-2.183-3.096-.45a.75.75 0 01-.564-.41L8 2.694v.001z'
-            />
-          </svg>
-          <span>PRO</span>
+          <div>
+            <svg
+              aria-hidden='true'
+              height='18'
+              viewBox='0 0 16 16'
+              version='1.1'
+              width='18'
+              data-view-component='true'
+              className='octicon octicon-star UnderlineNav-octicon hide-sm'
+            >
+              <path
+                fillRule='evenodd'
+                d='M8 .25a.75.75 0 01.673.418l1.882 3.815 4.21.612a.75.75 0 01.416 1.279l-3.046 2.97.719 4.192a.75.75 0 01-1.088.791L8 12.347l-3.766 1.98a.75.75 0 01-1.088-.79l.72-4.194L.818 6.374a.75.75 0 01.416-1.28l4.21-.611L7.327.668A.75.75 0 018 .25zm0 2.445L6.615 5.5a.75.75 0 01-.564.41l-3.097.45 2.24 2.184a.75.75 0 01.216.664l-.528 3.084 2.769-1.456a.75.75 0 01.698 0l2.77 1.456-.53-3.084a.75.75 0 01.216-.664l2.24-2.183-3.096-.45a.75.75 0 01-.564-.41L8 2.694v.001z'
+              />
+            </svg>
+            <span>PRO</span>
+          </div>
         </div>
       </aside>
     </React.StrictMode>
