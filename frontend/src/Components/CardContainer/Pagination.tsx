19804e1b948c417fdd92a389c47b26f239cbfa59
Jorge Moreno <jorgemoreno@MacBook-Pro-de-Jorge.local>
2022-01-15 13:39:44 -0600

    style(pagination): resize Icons
    Resize arrow Icons

--- frontend/src/Components/CardContainer/Pagination.tsx
+++ frontend/src/Components/CardContainer/Pagination.tsx
@@ -32,8 +32,8 @@ const Pagination: React.FC<PaginateProps> = ({
     <>
       <button disabled={page === 1 || page < 1} onClick={previousPage}>
         <svg
-          width='14'
-          height='27'
+          width='15'
+          height='25'
           viewBox='0 0 20 27'
           fill='none'
           xmlns='http://www.w3.org/2000/svg'
@@ -58,8 +58,8 @@ const Pagination: React.FC<PaginateProps> = ({
         onClick={nextPage}
       >
         <svg
-          width='14'
-          height='27'
+          width='15'
+          height='25'
           viewBox='0 0 20 27'
           fill='none'
           xmlns='http://www.w3.org/2000/svg'
