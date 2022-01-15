645a9a2d4f78b6e1a6ad6fcd9ee9ca6ddcac7592
Jorge Moreno <jorgemoreno@MacBook-Pro-de-Jorge.local>
2022-01-15 14:14:40 -0600

    feat(navbar): responsive Layout
    Add responsive menu icon, and styles to made responsive the navbar in any device

--- frontend/src/Components/Navbar/Navbar.tsx
+++ frontend/src/Components/Navbar/Navbar.tsx
@@ -6,6 +6,28 @@ const Navbar: React.FC = () => {
     <>
       <nav className='navbar'>
         <div className='navbar-container'>
+          <div className='navbar-container__responsive-icon'>
+            <details>
+              <summary>
+                <svg
+                  aria-hidden='true'
+                  height='24'
+                  viewBox='0 0 16 16'
+                  version='1.1'
+                  width='24'
+                  data-view-component='true'
+                  className='octicon octicon-three-bars'
+                >
+                  <path
+                    fillRule='evenodd'
+                    d='M1 2.75A.75.75 0 011.75 2h12.5a.75.75 0 110 1.5H1.75A.75.75 0 011 2.75zm0 5A.75.75 0 011.75 7h12.5a.75.75 0 110 1.5H1.75A.75.75 0 011 7.75zM1.75 12a.75.75 0 100 1.5h12.5a.75.75 0 100-1.5H1.75z'
+                  />
+                </svg>
+              </summary>
+            </details>
+          </div>
+        </div>
+        <div className='navbar-container'>
           <div className='navbar-container__logo'>
             <a href='/'>
               <svg
