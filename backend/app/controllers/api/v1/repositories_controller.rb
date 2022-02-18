89c76c62972163711e24903b3e5e6a0a98c4b7a9
Serlle Rosale <serlle@caraji.io>
2022-02-18 15:56:53 -0600

    refactor: change in the "Faraday" part

--- backend/app/controllers/api/v1/repositories_controller.rb
+++ backend/app/controllers/api/v1/repositories_controller.rb
@@ -4,15 +4,15 @@ module Api
   module V1
     class RepositoriesController < ApplicationController
       def index
-        conn = Faraday.new do |f|
+        conn = Faraday.new("https://api.github.com") do |f|
           f.request :authorization, 'Bearer', Figaro.env.GITHUB_TOKEN
           f.request :json # encode req bodies as JSON
           f.request :retry # retry transient failures
           f.response :follow_redirects # follow redirects
           f.response :json # decode response bodies as JSON
         end
-        user = conn.get("https://api.github.com/user?user=#{user_params}").body
-        repos = conn.get("https://api.github.com/user/repos?user=#{user_params}",
+        user = conn.get("/user?user=#{user_params}").body
+        repos = conn.get("/user/repos?user=#{user_params}",
                          { per_page: 100, sort: 'updated' }).body
         db_user = User.all.find { |u| u.github_id == user['id'] }
         if db_user.nil?
