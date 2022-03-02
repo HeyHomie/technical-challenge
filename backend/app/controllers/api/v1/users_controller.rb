1b5f88737b17225544d2ca985037bb7eb5b33130
Jose Orduno <Jose.m.orduno@gmail.com>
2022-03-01 19:02:40 -0800

    Modified wrong urls to make a request to githubapi

--- backend/app/controllers/api/v1/users_controller.rb
+++ backend/app/controllers/api/v1/users_controller.rb
@@ -11,9 +11,9 @@ module Api
           f.response :follow_redirects # follow redirects
           f.response :json # decode response bodies as JSON
         end
-        user = conn.get("https://api.github.com/user?user=#{user_params}").body
-        repos = conn.get("https://api.github.com/user/repos?user=#{user_params}", { per_page: 100 }).body
-        db_user = User.all.find { |u| u.github_id == user['id'] }
+        user = @conn.get("https://api.github.com/users/#{params[:login]}").body
+        repos = @conn.get("https://api.github.com/users/#{params[:login]}/repos",
+          db_user = User.all.find { |u| u.github_id == user['id'] }
         if db_user.nil?
           db_user = User.create({ github_id: user['id'], login: user['login'], url: user['html_url'], name: user['name'],
                                   email: user['email'], avatar_url: user['avatar_url'], repositories: repos })
