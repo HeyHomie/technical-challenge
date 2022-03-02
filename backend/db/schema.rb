9eb827e84f069070c01e41321e8908406d911d3b
Jose Orduno <Jose.m.orduno@gmail.com>
2022-03-01 16:02:50 -0800

    run repository migraiton

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,10 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_03_161853) do
+ActiveRecord::Schema.define(version: 2022_03_01_235439) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "repositories", force: :cascade do |t|
+    t.string "name"
+    t.string "language"
+    t.string "github_updated"
+    t.integer "github_id"
+    t.string "description"
+    t.string "url"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_repositories_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "login"
@@ -27,4 +40,5 @@ ActiveRecord::Schema.define(version: 2021_09_03_161853) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "repositories", "users"
 end
