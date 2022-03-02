449c4f3b19c2556ff48749058bdde8d783ae8317
Jose Orduno <Jose.m.orduno@gmail.com>
2022-03-01 19:08:51 -0800

    added routes for users and repositories

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,11 +1,12 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  resources :repositories
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      resources :users, only: [:index] do
-        resources :repositories, only: [:index]
+      resources :users, only: %i[index create show] do
+        resources :repositories, only: %i[index show]
       end
     end
   end
