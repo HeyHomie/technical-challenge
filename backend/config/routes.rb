82483920e781f10e5a04caa2b376fff5194f33a8
Jose Orduno <Jose.m.orduno@gmail.com>
2022-03-02 03:31:11 -0800

    remove index route from users resources

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,11 +1,10 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :repositories
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      resources :users, only: %i[index create show] do
+      resources :users, only: %i[create show] do
         resources :repositories, only: %i[index show]
       end
     end
