3f7f38c3d6b53f704d71afa392ce2a75d6508551
Jose Orduno <Jose.m.orduno@gmail.com>
2022-03-02 03:32:10 -0800

    rewrite repositories#show route to filter by name

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -5,7 +5,8 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users, only: %i[create show] do
-        resources :repositories, only: %i[index show]
+        resources :repositories, only: %i[index]
+        get 'repositories/:name', to: 'repositories#show', as: 'repository'
       end
     end
   end
