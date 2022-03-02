a0d5b613b5edd3944b15414c493722fa87221dec
Jose Orduno <Jose.m.orduno@gmail.com>
2022-03-01 19:18:20 -0800

    added tests to method user#show

--- backend/spec/requests/api/v1/users_spec.rb
+++ backend/spec/requests/api/v1/users_spec.rb
@@ -7,14 +7,34 @@ RSpec.describe 'Api::V1::UsersController', type: :request do
   let(:valid_user) { build(:user, login: 'yknx4') }
   let(:invalid_user) { build(:user, login: :fake_user) }
 
+  describe 'get /show' do
+    context 'if the user dont exists' do
+      it 'gets profile for fake user with id' do
+        get api_v1_user_path(id: Faker::Number.between(from: 1, to: 10))
+        expect(response).to have_http_status(:not_found)
+      end
+    end
+
+    context 'if the user exist in our db' do
+      it 'gets profile for fake user with id' do
+        get api_v1_user_path(id: saved_user.id)
+        body = JSON.parse(response.body)
+        expect(response).to have_http_status(:success)
+        expect(body['login']).to eq(saved_user.login)
+        expect(body['repositories']).to eq([])
+      end
+    end
+
+  end
+
   describe 'post /create' do
     context 'if the user exist in our db' do
-      it 'gets profile for fake user' do
+      it 'gets profile for fake user with login' do
         post api_v1_users_path(login: saved_user.login)
         body = JSON.parse(response.body)
         expect(response).to have_http_status(:success)
         expect(body['login']).to eq(saved_user.login)
-        expect(body['repositories']).to eq(nil)
+        expect(body['repositories']).to eq([])
       end
     end
 
