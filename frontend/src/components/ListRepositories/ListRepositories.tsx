e54622c262103d3423eb210ca618a84590dc585e
Daniel Teofilo <danyel.285309@gmail.com>
2022-03-03 02:40:25 -0600

    feat: listRepositories component created

---
+++ frontend/src/components/ListRepositories/ListRepositories.tsx
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from 'react'
+import './ListRepositories.css'
+import { FormSearch } from '../FormSearch/FormSearch'
+import { ItemResult } from '../ItemResult/ItemResult'
+import { filterSearch } from '../../helpers/filterSearch'
+
+interface Props {
+  repos: Array<any>
+}
+export const ListRepositories = ({ repos }: Props) => {
+  const [Repos, setRepos] = useState<Array<any>>([])
+  const [search, setSearch] = useState('')
+
+  useEffect(() => {
+    setRepos(repos)
+  }, [repos])
+
+  useEffect(() => {
+    if (search !== '') {
+      setRepos(filterSearch(Repos, search))
+    } else {
+      setRepos(repos)
+    }
+  }, [search])
+
+  const handleChangeInput = (e: any) => {
+    const { value } = e.target
+    setSearch(value)
+  }
+
+  return (
+    <div className="repositories-results">
+      <FormSearch handleChange={handleChangeInput} value={search} />
+
+      <div className="results">
+        <ul>
+          {Repos.map((item: any, index: number) => (
+            <ItemResult key={index} {...item} />
+          ))}
+        </ul>
+      </div>
+    </div>
+  )
+}
