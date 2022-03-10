389f116f868ae7c3a9877aab2db8f9f0cbde0aac
Jose Orduno <Jose.m.orduno@gmail.com>
2022-03-09 22:21:13 -0800

    remove unnecesary code for user_controller

--- backend/app/controllers/api/v1/users_controller.rb
+++ backend/app/controllers/api/v1/users_controller.rb
@@ -6,50 +6,19 @@ module Api
       before_action :find_user
 
       def show
-        render json: 'user not found', status: :not_found
-      end
-
-      def create
-        @conn = Faraday.new do |f|
-          f.request :authorization, 'Bearer', Figaro.env.GITHUB_TOKEN
-          f.request :json # encode req bodies as JSON
-          f.request :retry # retry transient failures
-          f.response :follow_redirects # follow redirects
-          f.response :json # decode response bodies as JSON
-        end
-        response = @conn.get("https://api.github.com/users/#{params[:login]}")
-        if response.status == 200
-          user = response.body
-          @user = User.new(github_id: user['id'], login: user['login'], url: user['html_url'],
-                           name: user['name'], email: user['email'], avatar_url: user['avatar_url'])
-          if @user.save
-            create_repositories
-            render json: @user
-          else
-            render json: @user.errors.full_messages, status: :not_found
-          end
-        else
-          render json: 'user not found', status: :not_found
-        end
+        response = UserService.new(user_params).call
+        render json: response.body, status: response.status
       end
 
       private
 
       def find_user
-        @user = User.where(login: params[:login]).or(User.where(id: params[:id])).last
+        @user = User.find_by(login: user_params)
         render json: @user if @user
       end
 
-      def create_repositories
-        repos = @conn.get("https://api.github.com/users/#{params[:login]}/repos",
-                          { per_page: 100 }).body
-        repos.each do |a|
-          @user.repositories.create(name: a['name'])
-        end
-      end
-
       def user_params
-        params.permit(:login, :id)
+        params.require(:login)
       end
     end
   end
