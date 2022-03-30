d9ada0d948527eac33a7043aa685ce205c231f4d
Serlle Rosale <serlle@caraji.io>
2022-03-30 14:42:57 -0600

    refactor(model repository): code change for better understanding
    I modify model because i have all it on object service

--- backend/app/models/repository.rb
+++ backend/app/models/repository.rb
@@ -9,23 +9,6 @@ class Repository < ApplicationRecord
   
   searchkick word_middle:[:name]
 
-  def self.save_data_base(db_user, db_repo, user, repo)
-    if db_user.nil? && db_repo.nil?
-      db_user = User.create({ github_id: user['id'], login: user['login'], url: user['html_url'], 
-                              name: user['name'], email: user['email'], avatar_url: user['avatar_url'] })  
-
-      db_repo = db_user.repositories.build({ repo_id: repo['id'], name: repo['name'], full_name: repo['full_name'],
-                                              private: repo['private'], owner: repo['owner'], 
-                                              html_url: repo['html_url'], visibility: repo['visibility'] })
-      db_repo.save
-    elsif db_repo.nil?
-      db_repo = db_user.repositories.build({ repo_id: repo['id'], name: repo['name'], full_name: repo['full_name'],
-                                              private: repo['private'], owner: repo['owner'], 
-                                              html_url: repo['html_url'], visibility: repo['visibility'] })
-      db_repo.save
-    end
-  end
-
   def searh_data
     {
       name: name
