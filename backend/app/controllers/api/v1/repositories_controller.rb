608dc24c0f49322aa16f74cab5d140e4746cd5b7
Serlle Rosale <serlle@caraji.io>
2022-03-30 14:33:29 -0600

    fix(repositories controller): i added class RepositoriesAll and before action
    I added a services object to refactor code in the controller, also I added pagination, search repos
    and before action to show index of the db

--- backend/app/controllers/api/v1/repositories_controller.rb
+++ backend/app/controllers/api/v1/repositories_controller.rb
@@ -2,43 +2,35 @@
 module Api
   module V1
     class RepositoriesController < ApplicationController
+      before_action :set_specific_repo, :set_repos
+
       def index
-        user = conn.get("/users/#{user_params}").body
-        repos = conn.get("/users/#{user_params}/repos", { per_page: 100, sort: 'updated' }).body
-        repos.each do |repo|
-          db_user = User.find_by(github_id: user['id'])
-          return render json: "The user does not exist or a problem occurred" if repos.include? "message"
-          db_repo = Repository.find_by(repo_id: repo['id'])
-          Repository.save_data_base(db_user, db_repo, user, repo)
-        end
-        db_user = User.find_by(github_id: user['id'])
-        # it search repo ----> URL.- http://localhost:3000/api/v1/users/NameTheUser/repositories?keyword=NameTheRepo
-        search = params[:keyword].present? ? params[:keyword] : nil
-        if search
-          Repository.reindex
-          repo_found = Repository.search search, where: { user_id: db_user.id }
-          return render json: repo_found.as_json
-        end
-        return render json: "The user does not exist or a problem occurred" if db_user.nil?
-        # it show 5 repositories to each page
-        # it shows all repositories with pagination. URL to each page.- http://localhost:3000/api/v1/users/NameTheUser/repositories?pages=NumberThePages
-        return render json: db_user.repositories.page(params[:page]).per(5).as_json if db_user
+        @db_repos = RepositoriesAll.call(user_params, @get_user_db)
+        set_specific_repo
+        render json: @db_repos.body.page(params[:page]).per(10).as_json unless @keyword
       end
 
       private
 
-      def user_params
-        params.require(:user_id)
+      def set_specific_repo
+        @get_user_db = User.find_by(login: user_params.downcase)
+        @get_repos_db = @get_user_db.repositories if @get_user_db
+        @keyword = params[:search].present? ? params[:search] : nil
+        unless @get_repos_db.blank?
+          if @keyword 
+            Repository.reindex
+            found_repo = Repository.search @keyword, where: { user_id: @get_user_db.id }
+            render json: found_repo.as_json
+          end
+        end
       end
 
-      def conn
-        Faraday.new("https://api.github.com") do |f|
-          #f.request :authorization, 'Bearer', Figaro.env.GITHUB_TOKEN
-          f.request :json # encode req bodies as JSON
-          f.request :retry # retry transient failures
-          f.response :follow_redirects # follow redirects
-          f.response :json # decode response bodies as JSON
-        end
+      def set_repos
+        render json: @get_repos_db.page(params[:page]).per(10).as_json unless @get_repos_db.blank?
+      end
+
+      def user_params
+        params.require(:user_id)
       end
     end
   end
