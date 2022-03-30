cadaeecdafd52c0a8fe39beebc5ae5143855533b
Serlle Rosale <serlle@caraji.io>
2022-03-30 13:25:27 -0600

    feat(crontrolle and service): add before action to user controller and user finder class
    I made the user controller code slimmer
    
    BREAKING CHANGE: I will make a service object called user finder

--- backend/app/controllers/api/v1/users_controller.rb
+++ backend/app/controllers/api/v1/users_controller.rb
@@ -3,32 +3,22 @@
 module Api
   module V1
     class UsersController < ApplicationController
-      def index
-        user = conn.get("/users/#{user_params}").body
-        db_user = User.find_by(github_id: user['id'])
-        return render json: "The user does not exist or a problem occurred" if user.values.include? "Not Found"
-        if db_user.nil?
-          db_user = User.create({ github_id: user['id'], login: user['login'], url: user['html_url'], name: user['name'],
-                                  email: user['email'], avatar_url: user['avatar_url'] })
-        end
-        # shows the searched public users.  URL.- http://localhost:3000/api/v1/users?user=NameTheUser
-        render json: db_user.as_json
+      before_action :set_user
+
+      def show
+        db_user = UserFinder.call(user_params)
+        render json: db_user.body.as_json
       end
 
       private
 
-      def user_params
-        params[:user].present? ? params[:user] : nil
+      def set_user
+        user = User.find_by(login: user_params.downcase)
+        render json: user.as_json if user
       end
 
-      def conn
-        Faraday.new("https://api.github.com") do |f|
-          #f.request :authorization, 'Bearer', Figaro.env.GITHUB_TOKEN
-          f.request :json # encode req bodies as JSON
-          f.request :retry # retry transient failures
-          f.response :follow_redirects # follow redirects
-          f.response :json # decode response bodies as JSON
-        end
+      def user_params
+        params.require(:id) #id = user name
       end
     end
   end
