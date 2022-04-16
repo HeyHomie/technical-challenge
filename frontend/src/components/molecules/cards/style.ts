57fd5d0163b1ca5cbf4e7686c126b1aa34037790
Sofia Herrera <sofia.herrera.ga@gmail.com>
2022-04-16 11:37:58 -0500

    feat(cards repositories): performs design and functionality of information cards of repositories
    performs design and functionality of informative cards of repositories with the requested data
    
    BREAKING CHANGE: performs design and functionality of informative cards

---
+++ frontend/src/components/molecules/cards/style.ts
@@ -0,0 +1,24 @@
+import { makeStyles } from '@mui/styles'
+
+export const useStyles = makeStyles(() => ({
+  label: {
+    display: 'inline-block',
+    padding: '0 7px',
+    fontSize: '12px',
+    fontWeight: 500,
+    lineHeight: '18px',
+    whiteSpace: 'nowrap',
+    border: '1px solid transparent',
+    borderRadius: '2em',
+    borderColor: '#d0d7de'
+  },
+  labelLanguage: {
+    position: 'relative',
+    top: '30%',
+    display: 'inline-block',
+    width: '12px',
+    height: '12px',
+    border: '1px solid #d0d7de',
+    borderRadius: '50%'
+  }
+}))
