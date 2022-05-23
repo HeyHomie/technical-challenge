349f704c4c3ac7e8b44fcfadb394894583dc8dc0
caeseste <caeseste@gmail.com>
2022-05-23 01:11:45 -0500

    fix: faraday requests

--- backend/app/controllers/api/v1/repositories_controller.rb
+++ backend/app/controllers/api/v1/repositories_controller.rb
@@ -4,15 +4,15 @@ module Api
   module V1
     class RepositoriesController < ApplicationController
       def index
-        conn = Faraday.new do |f|
-          f.request :authorization, 'Bearer', Figaro.env.GITHUB_TOKEN
+        conn = Faraday.new('https://api.github.com') do |f|
+          f.headers[:Authorization] = "token=#{ENV['GITHUB_TOKEN']}" if ENV['GITHUB_TOKEN'] # set auth header if token exists
           f.request :json # encode req bodies as JSON
           f.request :retry # retry transient failures
           f.response :follow_redirects # follow redirects
           f.response :json # decode response bodies as JSON
         end
-        user = conn.get("https://api.github.com/user?user=#{user_params}").body
-        repos = conn.get("https://api.github.com/user/repos?user=#{user_params}",
+        user = conn.get("/users/#{user_params}").body
+        repos = conn.get("/users/#{user_params}/repos",
                          { per_page: 100, sort: 'updated' }).body
         db_user = User.all.find { |u| u.github_id == user['id'] }
         if db_user.nil?
